896008add57af56ce80585dd24b0e4212a50c31d
deep-thot <jonatan@deep-thot.se>
2018-01-07 10:11:43 +0100

    Fix year generation going into a new year...

--- src/main/java/se/deepthot/playlistbot/theme/YearTheme.java
+++ src/main/java/se/deepthot/playlistbot/theme/YearTheme.java
@@ -1,8 +1,7 @@
 package se.deepthot.playlistbot.theme;
 
 import java.time.LocalDate;
-import java.time.temporal.WeekFields;
-import java.util.Locale;
+import java.time.temporal.ChronoUnit;
 import java.util.Optional;
 
 /**
@@ -12,14 +11,15 @@ public class YearTheme {
 
     private static final int initialYear = 2016;
     private static final int finalYear = 1950;
-    private static final int startWeek = 24;
+    private static final LocalDate startDate = LocalDate.of(2017, 6, 17);
 
     public static Optional<Integer> getCurrentYear(){
-        return Optional.of(initialYear - (getCurrentWeek() - startWeek)).filter(y -> y >= finalYear);
+
+        return Optional.of(initialYear - getCurrentWeek()).filter(y -> y >= finalYear);
     }
 
     private static int getCurrentWeek() {
-        return LocalDate.now().get(WeekFields.of(Locale.forLanguageTag("sv-SE")).weekOfWeekBasedYear());
+        return (int) startDate.until(LocalDate.now(), ChronoUnit.WEEKS);
     }
 
 
